bd7f7d9a7c4f962a04e462b95ca8a177dcfa9b00
Abhilash Manjunath <38949311+abhilash-24@users.noreply.github.com>
2024-01-18 08:07:53 +0000

    testing workflow

---
+++ src/main/java/com/example/sb_init_project/SbInitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_init_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbInitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbInitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sb_init_project/SbInitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_init_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbInitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
